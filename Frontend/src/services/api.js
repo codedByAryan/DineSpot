a07776e0cb1f4980d29b0cceabb89a9530ffeb90
Aryan@3001 <aryanchauhan9424@gmail.com>
2026-04-30 19:48:49 +0530

    pull data

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dinespot-backend-bv9s.onrender.com"
+  baseURL: "https://dinespot-backend-bv9s.onrender.com/api"
 });
 
 // Token automatically attach
